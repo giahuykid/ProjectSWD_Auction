ba9fa5ff3dae41557bb661509f4ddd6cd9ad8d63
Huy Tran <huytran@MacBookPro.lan>
2025-02-16 13:01:02 +0700

    oke

--- src/main/java/com/Huypro/SpringSecurity3/user/entity/UserEntity.java
+++ src/main/java/com/Huypro/SpringSecurity3/user/entity/UserEntity.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class UserEntity implements UserDetails {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String firstName;
     private String lastName;
